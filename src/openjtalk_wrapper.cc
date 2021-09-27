fb15fa529661d09ed175e020e2259e5d0750e20a
Yosshi999 <Yosshi999@users.noreply.github.com>
2021-09-27 12:35:10 +0000

    bugfix: pointer double free

--- src/openjtalk_wrapper.cc
+++ src/openjtalk_wrapper.cc
@@ -17,24 +17,16 @@
 
 namespace vvengine {
 struct OpenJtalkWrapper::Impl {
-  Mecab* mecab;
-  NJD* njd;
-  JPCommon* jpcommon;
+  Mecab mecab;
+  NJD njd;
+  JPCommon jpcommon;
 
-  Impl() {
-    mecab = new Mecab;
-    njd = new NJD;
-    jpcommon = new JPCommon;
-  }
+  Impl() : mecab(), njd(), jpcommon() {}
 
   ~Impl() {
-    Mecab_clear(mecab);
-    NJD_clear(njd);
-    JPCommon_clear(jpcommon);
-
-    delete mecab;
-    delete njd;
-    delete jpcommon;
+    Mecab_clear(&mecab);
+    NJD_clear(&njd);
+    JPCommon_clear(&jpcommon);
   }
 };
 
@@ -43,16 +35,16 @@ OpenJtalkWrapper::~OpenJtalkWrapper() {}
 bool OpenJtalkWrapper::Initialize() {
   BOOL mecabState;
 
-  mecabState = Mecab_initialize(impl->mecab);
-  NJD_initialize(impl->njd);
-  JPCommon_initialize(impl->jpcommon);
+  mecabState = Mecab_initialize(&impl->mecab);
+  NJD_initialize(&impl->njd);
+  JPCommon_initialize(&impl->jpcommon);
 
   return mecabState == TRUE;
 }
 
 bool OpenJtalkWrapper::Load(const char* mecabDir) {
   BOOL mecabState;
-  mecabState = Mecab_load(impl->mecab, mecabDir);
+  mecabState = Mecab_load(&impl->mecab, mecabDir);
   return mecabState == TRUE;
 }
 
@@ -62,25 +54,25 @@ void OpenJtalkWrapper::ExtractFullContext(const char* text,
   char buff[8192];
 
   text2mecab(buff, text);
-  mecabState = Mecab_analysis(impl->mecab, buff);
-  mecab2njd(impl->njd, impl->mecab->feature, impl->mecab->size);
-  njd_set_pronunciation(impl->njd);
-  njd_set_digit(impl->njd);
-  njd_set_accent_phrase(impl->njd);
-  njd_set_accent_type(impl->njd);
-  njd_set_unvoiced_vowel(impl->njd);
-  njd_set_long_vowel(impl->njd);
-  njd2jpcommon(impl->jpcommon, impl->njd);
-  JPCommon_make_label(impl->jpcommon);
+  mecabState = Mecab_analysis(&impl->mecab, buff);
+  mecab2njd(&impl->njd, impl->mecab.feature, impl->mecab.size);
+  njd_set_pronunciation(&impl->njd);
+  njd_set_digit(&impl->njd);
+  njd_set_accent_phrase(&impl->njd);
+  njd_set_accent_type(&impl->njd);
+  njd_set_unvoiced_vowel(&impl->njd);
+  njd_set_long_vowel(&impl->njd);
+  njd2jpcommon(&impl->jpcommon, &impl->njd);
+  JPCommon_make_label(&impl->jpcommon);
 
-  int labelSize = JPCommon_get_label_size(impl->jpcommon);
-  char** labelFeature = JPCommon_get_label_feature(impl->jpcommon);
+  int labelSize = JPCommon_get_label_size(&impl->jpcommon);
+  char** labelFeature = JPCommon_get_label_feature(&impl->jpcommon);
   labels.clear();
   for (int i = 0; i < labelSize; i++) labels.emplace_back(labelFeature[i]);
 
-  JPCommon_refresh(impl->jpcommon);
-  NJD_refresh(impl->njd);
-  Mecab_refresh(impl->mecab);
+  JPCommon_refresh(&impl->jpcommon);
+  NJD_refresh(&impl->njd);
+  Mecab_refresh(&impl->mecab);
 }
 
 }  // namespace vvengine
